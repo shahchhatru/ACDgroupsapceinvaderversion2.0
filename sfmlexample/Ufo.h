e9ee194070f70a2700677269c2b1f82a9314c66d
shahchhatru <55979766+shahchhatru@users.noreply.github.com>
2021-08-13 11:27:56 +0545

    Add files via upload

---
+++ sfmlexample/Ufo.h
@@ -0,0 +1,45 @@
+#ifndef UFO_H
+#define UFO_H
+
+#include"Bullet.h"
+#include"Player.h"
+#include <SFML/Graphics.hpp>
+
+
+class Ufo : public sf::Sprite
+{
+    public:
+        static int xarray[10];
+        static int yarray[10];
+        static int noobj;
+        static int score;
+        Ufo(sf::Texture& texture,int px,int py);
+        virtual ~Ufo();
+
+        int Getposx() { return posx; }
+        void Setposx(int val) { posx = val; }
+        int Getposy() { return posy; }
+        void Setposy(int val) { posy = val; }
+        int getxsize();
+
+        int getysize() ;
+        void move_down();
+        bool detect_collisions(Player &player);
+        bool detect_collisions(Bullet &player);
+        static void rand_number();
+        int getid();
+        void resetobj();
+
+    protected:
+
+    private:
+
+        int id;
+        int posx;
+        int posy;
+        int xsize;
+        int ysize;
+};
+
+
+#endif // UFO_H
